164b0a127a2a9a5d9b87ae7687c7411a02f7394d
mayur_kendre <kendremayur31@gmail.com>
2026-03-12 13:28:19 +0530

    add suspense boundry  to useSearchParams usage

--- src/app/(public)/error/page.tsx
+++ src/app/(public)/error/page.tsx
@@ -1,17 +1,25 @@
 "use client";
 
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react';
 
 const page = () => {
-   const params =  useSearchParams();
-   const error = params.get('error')
+
   return (
-    <div className='h-full w-full bg-zinc-200 flex items-center justify-center'>
+    <Suspense fallback= {<div className='h-full w-full flex items-center justify-center'><p className='animate-pulse'>Loading Error</p></div>}>
+      <div className='h-full w-full bg-zinc-200 flex items-center justify-center'>
         <div className='h-[50%] w-[70%] bg-red-300 text-red-500 flex items-center justify-center'>
-            <h1 className='text-2xl'>{error}</h1>
+            <Error/>
         </div>
     </div>
+    </Suspense>
   )
+}
+
+function Error (){
+     const params =  useSearchParams();
+   const error = params.get('error')
+   return <h1 className='text-2xl'>{error}</h1>
 }
 
 export default page
